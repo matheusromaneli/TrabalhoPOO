a2d155e6aa05be1acb42c86683179a65dfa5374f
DanSavino <savinodaniel@id.uff.br>
2021-05-01 22:56:44 -0300

    foda-se

--- Trabalho Restaurante/Restaurante/src/main/java/br/uff/ic/poo/restaurante/Cardapio/Cardapio.java
+++ Trabalho Restaurante/Restaurante/src/main/java/br/uff/ic/poo/restaurante/Cardapio/Cardapio.java
@@ -2,102 +2,165 @@ package br.uff.ic.poo.restaurante.Cardapio;
 
 import br.uff.ic.poo.restaurante.Item.Item;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.RandomAccessFile;
+import static java.lang.Float.parseFloat;
+import static java.lang.Integer.parseInt;
 import java.util.Scanner;
 
 public class Cardapio {   
     
     Scanner Teclado = new Scanner(System.in);
+    private int ocupado; //ocupado = 0 (LIVRE)  ocupado = 1 (OCUPADO)
     
-    public void SalvarItem(){
-        File arquivo = new File("src\\main\\java\\br\\uff\\ic\\poo\\restaurante\\Cardapio\\cardapio.txt");
+    public void SalvarItem() throws FileNotFoundException{
+        File arquivo = new File("src\\main\\java\\br\\uff\\ic\\poo\\restaurante\\Cardapio\\cardapio.dat");
         try{
             if (!arquivo.exists()) {
                 //cria arquivo vazio
                 arquivo.createNewFile();
                 
-                try (FileWriter fw = new FileWriter(arquivo, true); BufferedWriter bw = new BufferedWriter(fw)) {
+                try (FileOutputStream arqWrite = new FileOutputStream(arquivo)) {
+                    DataOutputStream gravarArq = new DataOutputStream(arqWrite);
                     
                     boolean continua;
                     continua = true;
-                    while (continua){                        
+                    while (continua){
                         Item novo = new Item();
-                        bw.write(novo.getId()+ " " + novo.getNome()+ " " +  + novo.getPreco());
-                        bw.newLine();
+                        this.ocupado = 1;
+                        gravarArq.writeUTF(novo.getId()+ ";" + novo.getNome()+ ";" +  + novo.getPreco() + ";" + this.ocupado + "\n");
                         System.out.println("Deseja salvar mais um item?");
                         String op = Teclado.nextLine();
                         if (("N".equals(op)) || ("n".equals(op))){
                             continua = false;
                         }   
                     }
-                }                  
+                    arqWrite.close();
+                }
             }else{
                 boolean continua;
-                continua = true;                
-                while (continua){
-                    //arquivo já contem dados
-                    System.out.println("\nInsira o nome do item a ser inserido");
-                    String nome = Teclado.nextLine();
-
-                    FileReader fr = new FileReader(arquivo);
-                    BufferedReader br = new BufferedReader(fr);
-                    boolean existe;
-                    existe = false;
-                    while (br.ready()) {
-                        //lê a linha
+                continua = true;  
+                try (FileInputStream arqLeitura = new FileInputStream(arquivo)) {
+                    DataInputStream lerArq = new DataInputStream(arqLeitura);
+                    while (continua){
+                        //arquivo já contem dados
+                        System.out.println("\nInsira o nome do item a ser inserido");
+                        String nome = Teclado.nextLine();                     
                         
-                        String linha = br.readLine();
-                        //verifica se item já encontra no arquivo
-                        String palavras[] = linha.split(" ");
-                        if (palavras[1].equals(nome)){
-                            existe = true;                       
-                        }                                                             
-                    }
-
-                    if (!existe){
-                        try (FileWriter fw = new FileWriter(arquivo, true); BufferedWriter bw = new BufferedWriter(fw)) {
+                        boolean existe;
+                        existe = false;
+                        int cont = 0;
+                        int tam = 0;
+                        while (arqLeitura.available() > 0) {
+                            //lê a linha
+                            String linha = lerArq.readUTF();
+                            //verifica se item já encontra no arquivo
+                            String palavras[] = new String[4];
+                            palavras = linha.split(";");
+                            if (palavras[1].equals(nome)){
+                                existe = true;
+                                tam = palavras[1].length();                                
+                            }
+                            cont++;
+                        }
+                        arqLeitura.close();
+                        if (!existe){
                             //caso não exista salvar novo item
-                            Item novo = new Item(nome);                            
-                            bw.write(novo.getId()+ " " + novo.getNome()+ " " +  + novo.getPreco());                           
-                            bw.newLine();                            
-                        }                    
-                    }else{
-                        System.out.println("\nItem já se encontra no cardapio");
+                            RandomAccessFile raf = new RandomAccessFile(arquivo,"rw");                        
+                            Item novo = new Item(nome);
+                            this.ocupado = 1;
+                            raf.seek(cont*(Integer.SIZE + (Character.SIZE * (tam+3)) + Float.SIZE + Integer.SIZE));
+                            raf.writeUTF(novo.getId()+ ";" + novo.getNome()+ ";" +  + novo.getPreco() + ";" + this.ocupado + "\n");
+                            raf.close();
+                        }else{
+                            System.out.println("\nItem já se encontra no cardapio");
+                        }
+                        System.out.println("Deseja salvar mais um item?");
+                        String op = Teclado.nextLine();
+                        if (("N".equals(op)) || ("n".equals(op))){
+                            continua = false;
+                        }
                     }
-                    System.out.println("Deseja salvar mais um item?");
-                    String op = Teclado.nextLine();
-                    if (("N".equals(op)) || ("n".equals(op))){
-                        continua = false;
-                    }        
-                }                              
+                }
             }                
         }catch (IOException ex) {
         }
         
     }
     
-    public void imprimeCardapio(){
-        File arquivo = new File("src\\main\\java\\br\\uff\\ic\\poo\\restaurante\\Cardapio\\cardapio.txt");
-        try{
-            FileReader fr = new FileReader(arquivo);
-            BufferedReader br = new BufferedReader(fr);
-            while (br.ready()) {
-                        //lê a linha
-                        String linha = br.readLine();
-                        //verifica se item já encontra no arquivo
-                        String palavras[] = linha.split(" ");
-                        System.out.println("ID:"+palavras[0]+ " Item: " + palavras[1]+ " R$" + palavras[2]);                                                                              
-                    }
-            
-        }catch (IOException ex) {
+    public void imprimeCardapio() throws FileNotFoundException, IOException{
+        File arquivo = new File("src\\main\\java\\br\\uff\\ic\\poo\\restaurante\\Cardapio\\cardapio.dat");
+        
+        try (FileInputStream arqLeitura = new FileInputStream(arquivo)) {
+            DataInputStream lerArq = new DataInputStream(arqLeitura);
+
+            while (arqLeitura.available() != 0){
+                //lê a linha
+                String linha = lerArq.readUTF();
+                //verifica se item já encontra no arquivo
+                String palavras[] = new String[4];
+                palavras = linha.split(";");
+                //palavras[3] é a flag de controle(ocupado)
+                if ("1".equals(palavras[3])){
+                    System.out.println("ID:" + palavras[0] + " Item:" + palavras[1] + " R$" + palavras[2]);
+                }                                                                                                      
+            }
         }
         
+       
     }
-      
+    
+    public void apagaItem() throws FileNotFoundException, IOException{        
+        File arquivo = new File("src\\main\\java\\br\\uff\\ic\\poo\\restaurante\\Cardapio\\cardapio.dat");
 
+        try (FileInputStream arqLeitura = new FileInputStream(arquivo)) {
+            DataInputStream lerArq = new DataInputStream(arqLeitura);
+
+            //arquivo já contem dados
+            System.out.println("\nInsira o nome do item a ser excluido");
+            String nome = Teclado.nextLine();                     
+
+            int pos, cont, tam;
+            tam = 0;
+            cont = 0;
+            pos = -1;
+            int idExcluida = -1;
+            float precoExcluido = -1; 
+            
+            
+            while ((arqLeitura.available() != 0)||(pos < 0)) {
+                //lê a linha
+                String linha = lerArq.readUTF();                
+                //verifica se item já encontra no arquivo
+                String palavras[] = linha.split(";");
+                if (palavras[1].equals(nome)){
+                    pos = cont;
+                    tam = palavras[1].length();
+                    idExcluida = parseInt(palavras[0]);
+                    precoExcluido = parseFloat(palavras[2]);                    
+                }
+                cont++;
+            }
+            arqLeitura.close();
+            try (RandomAccessFile raf = new RandomAccessFile(arquivo,"rw")) {
+                if (pos != -1){
+                    //salvar no arquivo nessa pos*tamanho de cada linha
+                    pos = pos*(Integer.SIZE + (Character.SIZE * (tam+3)) + Float.SIZE + Integer.SIZE);
+                    raf.seek(pos);
+                    this.ocupado = 0;
+                    raf.writeUTF(idExcluida + ";" + nome + ";" + precoExcluido + ";" + this.ocupado + "\n");
+                    System.out.println(nome + " foi excluido do cardapio");
+                }else{
+                    System.out.println(nome + " não se encontra no cardapio");
+                }
+                raf.close();
+            }
+        }        
+    }         
 }
--- Trabalho Restaurante/Restaurante/src/main/java/br/uff/ic/poo/restaurante/Main/main.java
+++ Trabalho Restaurante/Restaurante/src/main/java/br/uff/ic/poo/restaurante/Main/main.java
@@ -1,15 +1,193 @@
 package br.uff.ic.poo.restaurante.Main;
 
 import br.uff.ic.poo.restaurante.Cardapio.Cardapio;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Scanner;
 
 public class main {
-    public static void main(String[]args){
+    public static void main(String[] args){
+
+        Scanner teclado = new Scanner(System.in);
         
-        Cardapio cardapioAtual = new Cardapio();
-        cardapioAtual.imprimeCardapio();
-        cardapioAtual.SalvarItem();
-        cardapioAtual.imprimeCardapio();
-             
-    }   
+        System.out.println("\n........... Restaurant System Mega Super Pro Version 1.0.28.45 Fall 2021 ...........");
+
+        int escolha_menu_principal = -1;
+        
+        while(escolha_menu_principal != 0){
+
+            System.out.println("\nMenu principal: ");
+            System.out.println("1. Administrar cardápio.");
+            System.out.println("2. Iniciar funcionamento.");
+            System.out.println("0. Encerrar programa.\n");
+
+            escolha_menu_principal = teclado.nextInt();
+            
+            switch(escolha_menu_principal){
+                case 1:
+                    int escolha_menu_secundario1 = -1;
+
+                    while(escolha_menu_secundario1 != 0){
+
+                        System.out.println("\nAdministrar Cardápio: ");
+                        System.out.println("1. Imprimir cardápio.");
+                        System.out.println("2. Adicionar prato.");
+                        System.out.println("3. Remover prato.");
+                        System.out.println("0. Voltar ao menu anterior.\n");
+
+                        escolha_menu_secundario1 = teclado.nextInt();
+
+                        // instanciar cardápio
+
+                        switch(escolha_menu_secundario1){
+                            case 1:
+                                // chamar método para imprimir o cardápio
+                                break;
+                            case 2:
+                                // chamar método para adicionar prato ao cardápio
+                                break;
+                            case 3:
+                                // chamar método para remover prato do cardápio
+                                break;
+                            case 0:
+                                escolha_menu_secundario1 = 0;
+                                break;
+                            default:
+                                System.out.println("\nOpção inválida. Escolha uma opção válida.");
+                                break;
+                        }
+                    }
+                    break;
+                case 2:
+                    System.out.println("\nRestaurante em funcionamento.");
+                    
+                    int escolha_menu_secundario2 = -1;
+
+                    while(escolha_menu_secundario2 != 0){
+
+                        System.out.println("\nAdministrar salão: ");
+                        System.out.println("1. Abrir conta para mesa.");
+                        System.out.println("2. Fazer pedido para mesa.");
+                        System.out.println("3. Fechar conta para mesa.");
+                        System.out.println("4. Fazer pagamento de uma conta.");
+                        System.out.println("5. Administrar fila.");
+                        System.out.println("0. Voltar ao menu anterior.\n");
+
+                        escolha_menu_secundario2 = teclado.nextInt();
+
+                        switch(escolha_menu_secundario2){
+                            case 1:
+                                // instanciar uma mesa, adicioná-la às mesas existentes e abrir lista de pedidos para ela
+                                break;
+                            case 2:
+                                // chamar método para fazer pedido para uma mesa
+                                break;
+                            case 3:
+                                // fechar conta para uma mesa e apresentar valor total
+                                break;
+                            case 4:
+                                // apresentar formas de pagamento e receber pagamento (adicionar ao caixa do dia)
+                                break;
+                            case 5:
+                                // administrar a fila
+
+                                int escolha_menu_administrar_fila = -1;
+
+                                while(escolha_menu_administrar_fila != 0){
+                                    
+                                    System.out.println("\nAdministrar fila: ");
+                                    System.out.println("1. Adicionar nome à fila.");
+                                    System.out.println("2. Remover nome da fila.");
+                                    System.out.println("3. Mover nome para mesa livre.");
+                                    System.out.println("0. Voltar ao menu anterior.\n");
+
+                                    escolha_menu_administrar_fila = teclado.nextInt();
+
+                                    switch(escolha_menu_administrar_fila){
+                                        case 1:
+                                            // iniciar fila se não tiver sido ainda e adicionar nome no final dela
+                                            break;
+                                        case 2:
+                                            // remover um nome da fila
+                                            break;
+                                        case 3:
+                                            // mover pessoa para uma mesa vazia
+                                            break;
+                                        case 0:
+                                            escolha_menu_administrar_fila = 0;
+                                            break;
+                                        default:
+                                            System.out.println("\nOpção inválida. Escolha uma opção válida.");
+                                            break;
+                                    }
+                                }
+                                break;
+                            case 0:
+                                escolha_menu_secundario2 = 0;
+                                break;
+                            default:
+                                System.out.println("\nOpção inválida. Escolha uma opção válida.");
+                                break;
+                        }
+                    }
+                    break;
+                case 0:
+                    
+                    int sair_do_programa = -1;
+                    
+                    while(sair_do_programa != 0 && sair_do_programa != 1){
+                        
+                        System.out.println("\nVocê deseja encerrar o programa?");
+                        System.out.println("1. Sim.");
+                        System.out.println("0. Não.\n");
+                        
+                        sair_do_programa = teclado.nextInt();
+                        
+                        if(sair_do_programa != 0 && sair_do_programa != 1){
+                            System.out.println("\nOpção inválida. Escolha uma opção válida.");
+                        }
+                    }
+                    if(sair_do_programa == 0){
+                        escolha_menu_principal = -1;
+                    }else{
+                        System.out.print("\nEncerrando programa");
+                        try{
+                            Thread.sleep(800);
+                        }
+                        catch(InterruptedException ex){
+                            Thread.currentThread().interrupt();
+                        }
+                        System.out.print(".");
+                        try{
+                            Thread.sleep(800);
+                        }
+                        catch(InterruptedException ex){
+                            Thread.currentThread().interrupt();
+                        }
+                        System.out.print(".");
+                        try{
+                            Thread.sleep(800);
+                        }
+                        catch(InterruptedException ex){
+                            Thread.currentThread().interrupt();
+                        }
+                        System.out.println(".");
+                        try{
+                            Thread.sleep(400);
+                        }
+                        catch(InterruptedException ex){
+                            Thread.currentThread().interrupt();
+                        }
+                    }
+                    break;
+                default:
+                    System.out.println("\nOpção inválida. Escolha uma opção válida.");
+                    break;
+            }
+            
+        }
+        
+        System.out.println("\nPrograma encerrado.");
+    } 
     
 }
